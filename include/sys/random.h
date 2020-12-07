63a081235d3bfc5ea19edcc0cc5f614713223c38
chao.an <anchao@xiaomi.com>
2020-12-07 11:10:04 +0800

    libs/libc: correct the getrandom(2) prototype
    Reference here:
    https://man7.org/linux/man-pages/man2/getrandom.2.html
    
    Change-Id: I25b913c4b6533b3888cb2b9c62bb71505a0815c9
    Signed-off-by: chao.an <anchao@xiaomi.com>

--- include/sys/random.h
+++ include/sys/random.h
@@ -64,13 +64,21 @@
  * Input Parameters:
  *   bytes  - Buffer for returned random bytes
  *   nbytes - Number of bytes requested.
+ *   flags  - Bit mask that can contain zero or more of the ORed values
+ *            together.
  *
  * Returned Value:
- *   None
+ *   On success, getrandom() returns the number of bytes that were copied
+ *   to the buffer buf.  This may be less than the number of bytes
+ *   requested via buflen if either GRND_RANDOM was specified in flags and
+ *   insufficient entropy was present in the random source or the system
+ *   call was interrupted by a signal.
+ *
+ *   On error, -1 is returned, and errno is set appropriately.
  *
  ****************************************************************************/
 
-void getrandom(FAR void *bytes, size_t nbytes);
+ssize_t getrandom(FAR void *bytes, size_t nbytes, unsigned int flags);
 
 #endif /* CONFIG_CRYPTO_RANDOM_POOL */
 
